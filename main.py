ed17b6a61927940ba15e5917831303357b0c27f7
DEVANSH SHARMA <43249616+devanshsharma416@users.noreply.github.com>
2023-08-04 23:28:40 +0530

    Update main.py
    change the list1 function

--- main.py
+++ main.py
@@ -4,19 +4,22 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    list1 = [1, 2, 3, 4]
-    for i in list1:
-        print(i)
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_hi(list1):
+
+    if len(list1) == len(set(list1)):
+        return "List have unique elements"
+    else:
+        return "List have duplicate elements"
+    return -1
+
 def reverse_string(string):
     return string[::-1]
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    list1 = [1, 2, 3, 4]
+    print_hi(list1)
     print(reverse_string("Devansh"))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
